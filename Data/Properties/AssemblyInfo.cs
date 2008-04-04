d268da5025e0dff92013b3a84cb669fc47781981
zachary.yates <zachary.yates@a3753c16-114a-0410-a20a-976c5e22f1ab>
2008-04-04 19:33:46 +0000

    Added project

---
+++ Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SiteSmith Data Access Layer")]
+[assembly: AssemblyDescription("SiteSmith Data Access Layer")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("YatesMorrison Software, LLC.")]
+[assembly: AssemblyProduct("SiteSmith.Data")]
+[assembly: AssemblyCopyright("Copyright © 2008 YatesMorrison Software, LLC.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("45b093c6-e42e-4347-96d2-04d8f073983f")]
+
+//
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.*")]
---
+++ Security.Authentication/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SiteSmith Authentication")]
+[assembly: AssemblyDescription("SiteSmith Authentication")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("YatesMorrison Software, LLC.")]
+[assembly: AssemblyProduct("SiteSmith.Security.Authentication")]
+[assembly: AssemblyCopyright("Copyright © 2008 YatesMorrison Software, LLC.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("88e08bf4-f359-4dc9-807c-29ae8e37fa15")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
