0a57eb6e4cb07d5a20f1df845a31d7ebfe272655
zachary.yates <zachary.yates@a3753c16-114a-0410-a20a-976c5e22f1ab>
2008-04-10 03:00:01 +0000

    Finished image hanlder Image.ashx, fixed bugs in MediaService

--- trunk/Web/Include/Image.ashx.cs
+++ trunk/Web/Include/Image.ashx.cs
@@ -3,73 +3,135 @@
  * 4/5/2008 1:29:09 PM
  */
 
+using System;
+using System.Configuration;
+using System.Drawing;
+using System.IO;
+using System.Linq;
 using System.Web;
 using System.Web.Services;
-using System;
-using System.Linq;
-using System.Collections.Generic;
+
 using YatesMorrison.SiteSmith.Data;
-using System.IO;
-using System.Configuration;
+using YatesMorrison.SiteSmith.Service;
 
 namespace YatesMorrison.SiteSmith.Web
 {
 	/// <summary>
 	/// Retrieves an image based on it's ID and requested size
 	/// </summary>
-	[WebService(Namespace = "http://yatesmorrison.com/SiteSmith/")]
+	[WebService(Namespace = "http://yatesmorrison.com/SiteSmith/ImageHandler")]
 	[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
 	public class Image : IHttpHandler
 	{
 		public bool IsReusable { get { return true; } }
 
-		public void ProcessRequest(HttpContext context)
+		public void ProcessRequest(HttpContext httpContext)
 		{
 			// Retrieve the record
-			Guid mediaId = new Guid(context.Request.QueryString["Id"]);
-			ImageSize size = (ImageSize)int.Parse(context.Request.QueryString["Size"]);
+			Guid mediaId = new Guid(httpContext.Request.QueryString["Id"]);
+			ImageSize size = (ImageSize)int.Parse(httpContext.Request.QueryString["Size"]);
 
 			string fileExt = "";
 
-			using (SiteSmithDataContext context = new SiteSmithDataContext())
+			using (SiteSmithDataContext dataContext = new SiteSmithDataContext())
 			{
-				var media = context.Medias.Single(m => m.MediaId == mediaId);
+				var media = dataContext.Medias.Single(m => m.MediaId == mediaId);
 				fileExt = media.FileExtension;
 			}
 
 			// Build the correct file path
-			string folder = size.ToString();
-			string path = string.Format(@"{0}\{1}\{2}.{3}",
-				ConfigurationManager.AppSettings["MediaPath"],
-				folder,
-				mediaId.ToString(),
-				fileExt);
-
-			// Resize the image from the original if it doesn't exist
-			if (!File.Exists(path) && size != ImageSize.Original)
+			string originalPath = httpContext.Server.MapPath(BuildPath(mediaId, ImageSize.Original, fileExt));
+			string targetPath = httpContext.Server.MapPath(BuildPath(mediaId, size, fileExt));
+			
+			// Ensure the original image exists
+			if (File.Exists(originalPath))
 			{
+				if (size != ImageSize.Original)
+				{
+					Size targetSize = GetSizeFromConfiguration(size);
 
+					if (File.Exists(targetPath))
+					{
+						// Load the image into memory and make sure the dimensions are correct
+						Bitmap existingImage = Bitmap.FromFile(targetPath) as Bitmap;
+
+						if (existingImage.Width != targetSize.Width ||
+							existingImage.Height != targetSize.Height)
+						{
+							existingImage.Dispose();
+							existingImage = null;
+							// The dimensions are not correct, overwrite it with a resized image
+							new MediaService().Resize(originalPath, targetPath, targetSize.Width, targetSize.Height);
+						}
+					}
+					else
+					{
+						// No image! Create a new resized version
+						new MediaService().Resize(originalPath, targetPath, targetSize.Width, targetSize.Height);
+					}
+				}
+			}
+			else
+			{
+				throw new FileNotFoundException("Could not find: " + originalPath);
 			}
 
-			context.Response.ContentType = MimeTypes[Path.GetExtension(fileExt)];
-
+			// Should have all the files, write to the OutputStream
+			httpContext.Response.ContentType = GetMimeType(Path.GetExtension(fileExt));
+			httpContext.Response.WriteFile(targetPath);
+			httpContext.Response.Flush();
 		}
 
-		readonly Dictionary<string, string> MimeTypes = new Dictionary<string, string>()
+		#region Helper Methods
+
+		Size GetSizeFromConfiguration(ImageSize size)
 		{
-			{ "gif", "image/gif" },
-			{ "jpg", "image/jpeg" },
-			{ "jpeg", "image/jpeg" },
-			{ "png", "image/png" }
-		};
+			// TODO: Add error hanlding for the config entry
+			string configSize = ConfigurationManager.AppSettings[size.ToString() + "ImageSize"];
+			string[] dimensions = configSize.Split('x');
+			int width = int.Parse(dimensions[0]);
+			int height = int.Parse(dimensions[1]);
+			return new Size(width, height);
+		}
+
+		static string BuildPath(Guid mediaId, ImageSize size, string fileExt)
+		{
+			// Should produce MediaPath/ImageSize/Guid.FileExt
+			string path = string.Format(@"{0}/{1}/{2}.{3}",
+				ConfigurationManager.AppSettings["MediaPath"],
+				size.ToString(), 
+				mediaId.ToString(),
+				fileExt);
+			return path;
+		}
+
+		static string GetMimeType(string fileExt)
+		{
+			switch (fileExt.ToLowerInvariant())
+			{
+				default:
+				case "bmp":
+					return "image/bmp";
+
+				case "gif": return "image/gif";
+				
+				case "jpg":
+				case "jpeg":
+					return "image/jpeg";
+
+				case "png": return "image/png";
+			}
+		}
+
+		#endregion
 	}
 
 	public enum ImageSize
 	{
-		Thumbnail,
-		Original,
-		Small,
-		Medium,
-		Large
+		Original = 1,
+		Thumbnail = 2,
+		Small = 3,
+		Medium = 4,
+		Large = 5
 	}
 }
