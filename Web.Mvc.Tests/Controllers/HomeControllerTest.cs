ea730ea8474757d6f037c64c81bee21e5eb7a4fe
zachary.yates <zachary.yates@a3753c16-114a-0410-a20a-976c5e22f1ab>
2008-12-15 19:51:24 +0000

    Added Web.Mvc & Web.Mvc.Tests projects. Switching to an MVC model for SS2k8 web front end

---
+++ Web.Mvc.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Web.Mvc;
+using Web.Mvc.Controllers;
+
+namespace Web.Mvc.Tests.Controllers
+{
+	/// <summary>
+	/// Summary description for HomeControllerTest
+	/// </summary>
+	[TestClass]
+	public class HomeControllerTest
+	{
+		[TestMethod]
+		public void Index()
+		{
+			// Arrange
+			HomeController controller = new HomeController();
+
+			// Act
+			ViewResult result = controller.Index() as ViewResult;
+
+			// Assert
+			ViewDataDictionary viewData = result.ViewData;
+			Assert.AreEqual("Home Page", viewData["Title"]);
+			Assert.AreEqual("Welcome to ASP.NET MVC!", viewData["Message"]);
+		}
+
+		[TestMethod]
+		public void About()
+		{
+			// Arrange
+			HomeController controller = new HomeController();
+
+			// Act
+			ViewResult result = controller.About() as ViewResult;
+
+			// Assert
+			ViewDataDictionary viewData = result.ViewData;
+			Assert.AreEqual("About Page", viewData["Title"]);
+		}
+	}
+}
