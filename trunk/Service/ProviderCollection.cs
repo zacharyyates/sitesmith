5e1b687e01f1fdae0614c89d8d2cd3662111e6fe
zachary.yates <zachary.yates@a3753c16-114a-0410-a20a-976c5e22f1ab>
2008-12-15 20:26:52 +0000

    Added generic provider management

---
+++ trunk/Service/ProviderCollection.cs
@@ -0,0 +1,22 @@
+﻿/* Zachary Yates
+ * Copyright © 2008 YatesMorrison Software, LLC.
+ * 12/15/2008 3:25 PM
+ */
+
+using System.Configuration.Provider;
+
+namespace YatesMorrison.SiteSmith.Service
+{
+	/// <summary>
+	/// Returns the strongly typed collection of <see cref="ProviderBase"/> objects registered
+	/// </summary>
+	/// <seealso cref="ECastServiceProviderManager"/>
+	public class ProviderCollection<TProvider> : ProviderCollection
+		where TProvider : ProviderBase
+	{
+		new public TProvider this[string name]
+		{
+			get { return base[name] as TProvider; }
+		}
+	}
+}
