75fd583697cb851c5486fb62644bb41bb242db4a
zachary.yates <zachary.yates@a3753c16-114a-0410-a20a-976c5e22f1ab>
2008-12-15 20:26:52 +0000

    Added generic provider management

---
+++ Service/ProviderConfigurationSection.cs
@@ -0,0 +1,29 @@
+﻿/* Zachary Yates
+ * Copyright © 2008 YatesMorrison Software, LLC.
+ * 12/15/2008 3:26 PM
+ */
+
+using System.Configuration;
+
+namespace YatesMorrison.SiteSmith.Service
+{
+	/// <summary>
+	/// Custom configuration section for the generic provider
+	/// </summary>
+	/// <seealso cref="ECastServiceProviderManager"/>
+	public class ProviderConfigurationSection : ConfigurationSection
+	{
+		[ConfigurationProperty("providers")]
+		public ProviderSettingsCollection Providers
+		{
+			get { return (ProviderSettingsCollection)base["providers"]; }
+		}
+
+		[ConfigurationProperty("default", DefaultValue = "CachedProvider")]
+		public string Default
+		{
+			get { return (string)base["default"]; }
+			set { base["default"] = value; }
+		}
+	}
+}
---
+++ Service/ProviderManager.cs
@@ -0,0 +1,63 @@
+﻿/* Zachary Yates
+ * Copyright © 2008 YatesMorrison Software, LLC.
+ * 12/15/2008 3:25 PM
+ */
+
+using System;
+using System.Configuration;
+using System.Configuration.Provider;
+using System.Diagnostics;
+using System.Web.Configuration;
+
+namespace YatesMorrison.SiteSmith.Service
+{
+	/// <summary>
+	/// Standard provider pattern implementation
+	/// </summary>
+	public static class ProviderManager<TProvider, TProviderConfigurationSection>
+		where TProvider : ProviderBase
+		where TProviderConfigurationSection : ProviderConfigurationSection
+	{
+		static TProvider s_Provider;
+		static ProviderCollection<TProvider> s_Providers;
+
+		static ProviderManager()
+		{
+			try
+			{
+				TProviderConfigurationSection configuration =
+                     ConfigurationManager.GetSection(typeof(TProvider).Name) as TProviderConfigurationSection;
+
+				if (configuration == null)
+					throw new ConfigurationErrorsException("ConfigurationSection is null");
+
+				s_Providers = new ProviderCollection<TProvider>();
+
+				ProvidersHelper.InstantiateProviders(
+					configuration.Providers, s_Providers, typeof(TProvider));
+
+				s_Providers.SetReadOnly();
+
+				s_Provider = s_Providers[configuration.Default];
+
+				if (s_Provider == null)
+					throw new ConfigurationErrorsException("The default provider is null");
+			}
+			catch (Exception error)
+			{
+				Trace.TraceError(error.ToString());
+				throw new ConfigurationErrorsException(error.Message, error);
+			}
+		}
+
+		public static TProvider Provider
+		{
+			get { return s_Provider; }
+		}
+
+		public static ProviderCollection<TProvider> Providers
+		{
+			get { return s_Providers; }
+		}
+	}
+}
